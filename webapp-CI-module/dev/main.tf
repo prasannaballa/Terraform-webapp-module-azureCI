0797de2711ef94ab82780f107b33d61afad1b9dc
prasannaballa <128133786+prasannaballa@users.noreply.github.com>
2023-10-26 10:15:39 +0530

    Update main.tf

--- webapp-CI-module/dev/main.tf
+++ webapp-CI-module/dev/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "aks"
+    storage_account_name = "webdevci"
+    container_name = "webci"
+    key = "terraform.storage"
+    access_key = "z8GC6s11FEQE13cRuYF5jSKBSWVCsbb4auh83h2d5El7RST9DVMEH00whWkbfKiRhhkKJx6SO7tE+AStkF2a4A=="
+  }
+}
 module "dev" {
   source      = "../module"
   prefix = "dev"
