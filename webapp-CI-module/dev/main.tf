2ced8df222463218cacb096024cae038bbc421bb
prasannaballa <128133786+prasannaballa@users.noreply.github.com>
2023-10-26 19:49:44 +0530

    Update main.tf

--- webapp-CI-module/dev/main.tf
+++ webapp-CI-module/dev/main.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "aks"
-    storage_account_name = "webdevci"
-    container_name = "webci"
+    resource_group_name = "sivaaks"
+    storage_account_name = "webacr"
+    container_name = "webacrmodule"
     key = "terraform.storage"
-    access_key = "z8GC6s11FEQE13cRuYF5jSKBSWVCsbb4auh83h2d5El7RST9DVMEH00whWkbfKiRhhkKJx6SO7tE+AStkF2a4A=="
+    access_key = "SP7p/X+ZnFhAoufkEaBk+4cqKJdhIQpbrITUiQD5g1mWi2vLhbLxYdcMPafRIbULWBVrXdavUMjs+AStdYHRdg=="
   }
 }
 module "dev" {
